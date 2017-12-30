311a73073a0143256e803aa6157da3514b82caff
Divya Singh <ds492015@gmail.com>
2017-12-30 10:27:22 +0530

    deleted game.js

--- src/game.js
+++ src/game.js
@@ -1,13 +1,7 @@
 const Game = function(numberOfRows, numberOfCols) {
   this.rows = numberOfRows;
   this.cols = numberOfCols;
-  this.snake = {};
 }
-
-Game.protype.createFood = function() {
-  food=generateRandomPosition(this.rows,this.cols);
-};
-
 
 Game.prototype.hasSnakeTouchBorder = function(position) {
  let maxCoordX = numberOfRows-1;
